86c8410f4331a1a828de9b207af47979eea7886a
morerii <moreridaffa24@gmail.com>
2021-02-23 00:01:00 +0700

    title bar

---
+++ app/src/main/java/com/blogspot/yourfavoritekaisar/pangansehat/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.blogspot.yourfavoritekaisar.pangansehat.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blogspot.yourfavoritekaisar.pangansehat.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
